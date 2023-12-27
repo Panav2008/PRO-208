2b7701f55e55ae8e7c402ab6478ad1bbb4ea55f2
Panav2008 <68947341+Panav2008@users.noreply.github.com>
2023-12-27 19:51:27 +0530

    Add files via upload

---
+++ PRO-208/client.py
@@ -0,0 +1,16 @@
+import socket
+from threading import Thread
+from tkinter import*
+from tkinter import ttk
+from tkinter import filedialog
+IP_ADDRESS='127.0.0.1'
+PORT=8050
+SERVER=None
+BUFFER_SIZE=4096
+def setup():
+    global PORT
+    global IP_ADDRESS
+    global SERVER
+    SERVER=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    SERVER.connect((IP_ADDRESS,PORT))
+setup()
